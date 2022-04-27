f60a49a17e58ee42740e8c518aa3fe10d9e32d4e
wtrumanrose <wtrumanrose@gmail.com>
2022-04-27 17:21:32 -0600

    Initial tokenizing, removal of stop words, and stemming.

--- Code/02_data-wrangling.R
+++ Code/02_data-wrangling.R
@@ -25,13 +25,59 @@ joint_data <- read_rds(here::here("Data", "joint_data.rds"))
 # - Keep all the group information as you tokenize.
 
 # Los Tokens
-text_tokens <- text_tibble %>% unnest_tokens(word, text, token = "words")
+
+word_tokens <- call_data %>%
+  mutate(text= str_replace_all(text, "\\.", " ")) %>%
+  mutate(text= str_replace_all(text, "\\b", " ")) %>%
+  unnest_tokens(word, text, token = "words")
 
 # Remove Stop Words -------------------------------------------------------
 # - stopwords data frame needs to be looked at in tidytext.
 # - specialized stopwords .txt file provided by the dictionary authors.
 
+# Import LM Stopword lists, generic is contained within generic_long
+
+sw_auditor <- read_csv(here::here("Data", "stopwords_lm_auditor.csv"))
+# sw_currency <- read_csv(here::here("Data", "stopwords_lm_currency.csv"))
+sw_dates_numbers <- read_csv(here::here("Data", "stopwords_lm_dates_numbers.csv"))
+sw_generic_long <- read_csv(here::here("Data", "stopwords_lm_generic_long.csv"))
+# sw_generic <- read_csv(here::here("Data", "stopwords_lm_generic.csv"))
+sw_geography <- read_csv(here::here("Data", "stopwords_lm_geography.csv"))
+sw_names <- read_csv(here::here("Data", "stopwords_lm_names.csv"))
+
+lm_stopwords <- sw_auditor %>% 
+  bind_rows(sw_dates_numbers, sw_generic_long, sw_geography, sw_names) %>% 
+  mutate(word= str_to_lower(word))
+  
+
+# Loughran McDonald stop words
+word_tokens_lm <- word_tokens %>% anti_join(lm_stopwords)
+
+# Tidytext stop words
+word_tokens_tt <- word_tokens %>% anti_join(stop_words)
+
+# Both lists
+word_tokens_both <- word_tokens_lm  %>% anti_join(stop_words)
+
+
+wordcount <- word_tokens_both %>% count(word, sort= TRUE)
+
+# Need to deal with contractions ('ll, 've, 't, 's, 'd)
+
 # Stemming ----------------------------------------------------------------
+
+# SMLTA Book isn't a big proponent of stemming and offers some published
+# evidence that it can even be counterproductive. Still, I'll include code,
+# but it may not be used
+
+# Using SnowballC
+stem_tokens_snowball <- word_tokens_both %>% 
+  mutate(stems = SnowballC::wordStem(word))
+
+# Using hunspell--note how it drops essentially everything that isn't a word.
+# Probably not a good call for what we're doing
+stem_tokens_hunspell <- word_tokens_both %>%
+  mutate(stems = hunspell::hunspell_stem(word))
 
 # Word Embeddings ---------------------------------------------------------
 
