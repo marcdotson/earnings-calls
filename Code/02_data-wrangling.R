c7c1cbd8e1c9321eec4004ad8f7c98dd060bf2b1
wtrumanrose <wtrumanrose@gmail.com>
2022-04-28 11:18:39 -0600

    Word embeddings, document embeddings, word embedding visualisation with the most filtered data

--- Code/02_data-wrangling.R
+++ Code/02_data-wrangling.R
@@ -1,7 +1,12 @@
 # Tokenizing --------------------------------------------------------------
-# Load packages.
+# Load packages
+# Need widyr, furrr, slider, and irlba.
 library(tidyverse)
 library(tidytext)
+library(widyr)
+library(furrr)
+library(irlba)
+
 
 # Read data.
 call_data <- read_rds(here::here("Data", "call_data.rds"))
@@ -63,6 +68,11 @@ word_tokens_both <- word_tokens_lm  %>% anti_join(stop_words)
 wordcount <- word_tokens_both %>% count(word, sort= TRUE)
 
 # Need to deal with contractions ('ll, 've, 't, 's, 'd)
+# I'll just remove them for now
+
+cont <- list("ll", "ve", "t", "s", "d")
+
+word_tokens_both <- word_tokens_both %>% filter(!(word %in% cont))
 
 # Stemming ----------------------------------------------------------------
 
@@ -81,3 +91,100 @@ stem_tokens_hunspell <- word_tokens_both %>%
 
 # Word Embeddings ---------------------------------------------------------
 
+# Word embeddings seem to occupy a space somewhere between data wrangling and
+# modeling. They are computationally intensive and require a fair bit of
+# judgment. The process used to make word embeddings is outlined in SMLTA
+# I won't use the stemmed words for this, unless I find a compelling reason to
+
+# Add counts, filter by 10 or up 
+word_tokens_both <- word_tokens_both %>% 
+  add_count(word) %>%
+  filter(n >= 10) %>%
+  select(-n)
+
+# Nest the words
+nested_words <- word_tokens_both %>%
+  nest(words = c(word))
+
+# Creating slide windows, which are used to calculate skipgram probabilities
+
+
+slide_windows <- function(tbl, window_size) {
+  skipgrams <- slider::slide(
+    tbl, 
+    ~.x, 
+    .after = window_size - 1, 
+    .step = 1, 
+    .complete = TRUE
+  )
+  
+  safe_mutate <- safely(mutate)
+  
+  out <- map2(skipgrams,
+              1:length(skipgrams),
+              ~ safe_mutate(.x, window_id = .y))
+  
+  out %>%
+    transpose() %>%
+    pluck("result") %>%
+    compact() %>%
+    bind_rows()
+}
+
+
+plan(multisession)  ## for parallel processing
+
+# Not exactly sure how to explain what I'm doing with this, which isn't great
+# 8L for 8 cores! Didn't take long to run though, even if you have 4 cores
+
+tidy_pmi <- nested_words %>%
+  mutate(words = future_map(words, slide_windows, 8L)) %>%
+  unnest(words) %>%
+  unite(window_id, title, window_id) %>%
+  pairwise_pmi(word, window_id)
+
+
+# Creating the vectors
+
+tidy_word_vectors <- tidy_pmi %>%
+  widely_svd(
+    item1, item2, pmi,
+    nv = 100, maxit = 1000
+  )
+
+# Hurray!
+
+
+# Function from SMLTA to find the nearest neighbor of words
+
+nearest_neighbors <- function(df, token) {
+  df %>%
+    widely(
+      ~ {
+        y <- .[rep(token, nrow(.)), ]
+        res <- rowSums(. * y) / 
+          (sqrt(rowSums(. ^ 2)) * sqrt(sum(.[token, ] ^ 2)))
+        
+        matrix(res, ncol = 1, dimnames = list(x = names(res)))
+      },
+      sort = TRUE
+    )(item1, dimension, value) %>%
+    select(-item2)
+}
+
+
+nearest_neighbors(tidy_word_vectors, "q1")
+
+
+# Document embeddings
+
+word_matrix <- word_tokens_both %>%
+  count(title, word) %>%
+  cast_sparse(title, word, n)
+
+embedding_matrix <- tidy_word_vectors %>%
+  cast_sparse(item1, dimension, value)
+
+doc_matrix <- word_matrix %*% embedding_matrix
+
+dim(doc_matrix)
--- Code/03_visualizations.R
+++ Code/03_visualizations.R
@@ -1,6 +1,33 @@
 # Mostly unsupervised visualizations.
 
 # Overall word count.
+
+
+
+
 # Overall word count by year.
 
 # Word embeddings visualizations or as part of modeling?
+
+
+
+# More code from SMLTA
+
+tidy_word_vectors %>%
+  filter(dimension <= 24) %>%
+  group_by(dimension) %>%
+  top_n(12, abs(value)) %>%
+  ungroup() %>%
+  mutate(item1 = reorder_within(item1, value, dimension)) %>%
+  ggplot(aes(item1, value, fill = dimension)) +
+  geom_col(alpha = 0.8, show.legend = FALSE) +
+  facet_wrap(~dimension, scales = "free_y", ncol = 4) +
+  scale_x_reordered() +
+  coord_flip() +
+  labs(
+    x = NULL,
+    y = "Value",
+    title = "Apple Earnings Calls",
+    subtitle = paste("Top words contributing to the components that explain",
+                     "the most variation")
+  )
