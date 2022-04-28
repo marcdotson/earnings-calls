6e2b7bb5d419c8130c5c25e730e796b978f9fcc4
wtrumanrose <wtrumanrose@gmail.com>
2022-04-28 15:35:04 -0600

    Filtered data through more stop word lexicons, change embeddings to run with no stop words filtered, prepped for visualisations, though that code could go in data wrangling

--- Code/03_visualizations.R
+++ Code/03_visualizations.R
@@ -2,6 +2,94 @@
 
 # Overall word count.
 
+# Word counts after filtering with different stop word lexicons
+
+# No stop words
+no_stopwords <- word_tokens %>%
+  group_by(year, quarter) %>% 
+  count(word) %>% 
+  arrange(desc(n)) %>% 
+  mutate(stopwords = "no stopwords")
+
+# Loughran McDonald stop words
+lm_stopwords <- word_tokens_lm %>% 
+  group_by(year, quarter) %>% 
+  count(word) %>% 
+  arrange(desc(n)) %>% 
+  mutate(stopwords = "lm")
+
+# Tidytext stop words (SMART, Snowball, ISO)
+tidytext_stopwords <- word_tokens_tt %>% 
+  group_by(year, quarter) %>% 
+  count(word) %>% 
+  arrange(desc(n)) %>% 
+  mutate(stopwords = "tidytext")
+
+# SMART stop words
+smart_stopwords <- word_tokens_smart %>% 
+  group_by(year, quarter) %>% 
+  count(word) %>% 
+  arrange(desc(n)) %>% 
+  mutate(stopwords = "smart")
+
+# Snowball stop words
+snowball_stopwords <- word_tokens_snowball %>% 
+  group_by(year, quarter) %>% 
+  count(word) %>% 
+  arrange(desc(n)) %>% 
+  mutate(stopwords = "snowball")
+
+# ISO stop words
+iso_stopwords <- word_tokens_iso %>% 
+  group_by(year, quarter) %>% 
+  count(word) %>% 
+  arrange(desc(n)) %>% 
+  mutate(stopwords = "iso")
+
+# onix stop words
+onix_stopwords <- word_tokens_onix %>% 
+  group_by(year, quarter) %>% 
+  count(word) %>% 
+  arrange(desc(n)) %>% 
+  mutate(stopwords = "onix")
+
+# All stop word lexicons
+all_stopwords <- word_tokens_all %>% 
+  group_by(year, quarter) %>% 
+  count(word) %>% 
+  arrange(desc(n)) %>% 
+  mutate(stopwords = "all")
+ 
+# Binding previous data frames, grouped by year and quarter
+word_counts <- no_stopwords %>% 
+  bind_rows(tidytext_stopwords,
+            lm_stopwords,
+            smart_stopwords,
+            iso_stopwords,
+            snowball_stopwords,
+            onix_stopwords,
+            all_stopwords)
+
+# Word totals, grouped by word/stop word
+word_tot <- word_counts %>%
+  ungroup() %>% 
+  select(word, n, stopwords) %>% 
+  group_by(word, stopwords) %>% 
+  summarise(totals = sum(n))
+
+
+# Word count, no stop words
+word_tot %>% 
+  select(word, totals) %>%
+  ungroup %>% 
+  distinct() %>% 
+  arrange(desc(totals)) %>%
+  slice(1:30) %>% 
+  mutate(word = fct_reorder(word, totals)) %>%
+  ggplot(aes(x = totals, y = word)) +
+  geom_col()
+  
+
 
 
 
