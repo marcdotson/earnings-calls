460c6e3b379ae1fbc0ac1901e185b3e94b279bb0
Marc Dotson <marc_dotson@byu.edu>
2022-05-18 19:22:17 -0600

    Data import rewritten to extract quarter and year from the title and the text

--- Code/01_import-data.R
+++ Code/01_import-data.R
@@ -2,89 +2,90 @@
 # Load libraries.
 library(tidyverse)
 
-# transcripts <- readtext::readtext(str_c("/Volumes/ELDER/Conference Call Project/Conference Calls Text Files/", "*.txt"))
-# write_rds(transcripts, here::here("Data", "transcripts.rds"))
-
-transcripts <- read_rds(here::here("Data", "transcripts.rds"))
-
-# Confirm that we can filter on no "quarter" in title. So far, only 
-# transcripts that aren't quarterly earnings calls don't have some
-# reference to quarter in their titles.
-
-# - Many transcripts don't have the quarter and year in the title.
-# - Look at the first lines of the actual transcript for information.
-# - Filter on words that indicate a non-quarter earnings call:
-# abstract?, event brief?, interim?, full year?, etc.
-
-# readtext::readtext(here::here("Data", "*.txt")) %>%
-transcripts %>%
-  tibble() %>%
-  separate(doc_id, into = c("gvkey", "call_date", "title"), sep = "_") %>%
-  mutate_if(is.character, ~gsub('[^ -~]', '', .)) %>%
-  mutate(
-    title = str_to_lower(title),
-    text = str_trunc(text, 100),
-    quarter = str_extract(title, "q\\d|(\\w+)(?=\\squarter)")
-  ) %>%
-  filter(grepl("earnings", title), grepl("call", title)) %>% 
-  # filter(is.na(quarter)) %>%
-  select(title, quarter) %>%
-  as.data.frame() %>% 
-  write_csv(here::here("Private", "filter_test_01.csv"))
-
-# Import all .txt files in Data.
-call_data <- #readtext::readtext(here::here("Data", "*.txt")) %>% 
-  transcripts %>%
+# Import all earning call transcripts, separate the doc_id, and remove non-UTF-8 characters.
+transcripts <- read_rds(here::here("Data", "transcripts.rds")) %>% 
   tibble() %>% 
-  separate(doc_id, into = c("gvkey", "call_date", "title"), sep = "_") %>% 
+  separate(doc_id, into = c("gvkey", "call_date", "title"), sep = "_")
+
+# Clean and filter so we have transcripts that have the correct quarter and year.
+call_data <- transcripts %>%
   mutate(
-    # Extract year and quarter from the title and remove carriage returns.
-    call_date = lubridate::mdy(call_date),
-    title = str_to_lower(title),
-    year = str_extract(title, "20\\d\\d|(?<=q\\d\\s)(\\d\\d)|(?<=fy\\s)(\\d\\d)"),
-    quarter = str_extract(title, "q\\d|(\\w+)(?=\\squarter)"),
-    text = str_replace_all(text, "\r?\n|\r", " ")
+    title = str_to_lower(title),                                     # Make titles lowercase.
+    text = str_replace_all(text, "\r?\n|\r", " "),                   # Remove carriage returns.
+    first_line = str_trunc(text, 1000) %>% str_to_lower()            # Extract the first line.
   ) %>% 
-  # Filter on no "quarter" along with "Abstract|Event Brief" in title.
-  drop_na(quarter) %>%
-  filter(!grepl("abstract|event brief", title)) %>% 
-  # Remove non-UTF-8 characters.
-  mutate_if(is.character, ~gsub('[^ -~]', '', .)) %>%
+  # Filter transcripts that are unlikely to be earning calls.
+  anti_join(
+    bind_rows(
+      filter(temp, grepl("abstract|(event brief)", title)),          # Filter transcripts with "abstract" or "event brief".
+      filter(temp, grepl(str_c(
+        "(full year)|annual", "|",                                   # Filter transcripts with "full year", "annual" or
+        "preliminary|interim|fiscal", "|",                           # "preliminary", "interim", "fiscal" or
+        "(half year)|(year end)|yearend"                             # "half year", "year end", or "yearend"
+      ), title), !grepl("quarter|q\\d|q\\d\\d|\\dq|\\d\\dq", title)) # but without "quarter", "q#", "q##", "#q", "##q".
+    ), 
+    by = c("gvkey", "call_date", "title")
+  ) %>%
+  # Extract the quarter from the title or the beginning of the text.
   mutate(
-    # Use the call_date for year if it isn't present in the title.
-    year = ifelse(is.na(year), lubridate::year(call_date), year),
-    # Clean up year and quarter.
+    call_date = lubridate::mdy(call_date),
+    quarter = str_extract(title, str_c(
+      "((\\w+)(?=\\squarter))", "|",                                 # Extract word before "quarter" or
+      "((\\w+)(?=-quarter))", "|",                                   # word before "-quarter" or
+      "((?<=quarter-\\s)(\\w+)|(\\d))", "|",                         # word or "#" after "quarter-" or
+      "((q\\d)|(q\\d\\d)|(\\dq)|(\\d\\dq))"                          # "q#", "q##", "#q", "##q".
+    )),
+    quarter = case_when(
+      str_detect(quarter, "first|1st|q1|q01|1q|01q") ~ "1",
+      str_detect(quarter, "second|2nd|q2|q02|2q|02q") ~ "2",
+      str_detect(quarter, "third|3rd|q3|q03|3q|03q") ~ "3",
+      str_detect(quarter, "fourth|4th|q4|q04|4q|04q") ~ "4"
+    ),
+    quarter = ifelse(!is.na(quarter), quarter, str_extract(first_line, str_c(
+      "((\\w+)(?=\\squarter))", "|",                                 # Extract word before "quarter" or
+      "((\\w+)(?=-quarter))", "|",                                   # word before "-quarter" or
+      "((?<=quarter-\\s)(\\w+)|(\\d))", "|",                         # word or "#" after "quarter-" or
+      "((q\\d)|(q\\d\\d)|(\\dq)|(\\d\\dq))"                          # "q#", "q##", "#q", "##q".
+    ))),
+    quarter = case_when(
+      str_detect(quarter, "1|first|1st|q1|q01|1q|01q") ~ "1",
+      str_detect(quarter, "2|second|2nd|q2|q02|2q|02q") ~ "2",
+      str_detect(quarter, "3|third|3rd|q3|q03|3q|03q") ~ "3",
+      str_detect(quarter, "4|fourth|4th|q4|q04|4q|04q") ~ "4"
+    ),
+    # Extract the year from the title or the beginning of the text.
+    year = str_extract(title, str_c(
+      "(20\\d\\d)", "|",                                             # Extract year "20##" or
+      "((?<=q\\d\\s)(\\d\\d))", "|",                                 # "##" after "q#" or
+      "((?<=q\\d\\d\\s)(\\d\\d))", "|",                              # "##" after "q##" or
+      "((?<=\\dq\\s)|(?<=\\dq)(\\d\\d))", "|",                       # "##" after "#q" or
+      "((?<=\\d\\dq\\s)|(?<=\\d\\dq)(\\d\\d))", "|",                 # "##" after "##q" or
+      "((?<=')(\\d\\d))", "|",                                       # "##" after "'" or
+      "((?<=fy\\s)(\\d\\d)|(?<=fy)(\\d\\d))"                         # "##" after "fy " or "fy".
+    )),
+    year = ifelse(!is.na(year), year, str_extract(first_line, str_c(
+      "(20\\d\\d)", "|",                                             # Extract year "20##" or
+      "((?<=q\\d\\s)(\\d\\d))", "|",                                 # "##" after "q#" or
+      "((?<=q\\d\\d\\s)(\\d\\d))", "|",                              # "##" after "q##" or
+      "((?<=\\dq\\s)|(?<=\\dq)(\\d\\d))", "|",                       # "##" after "#q" or
+      "((?<=\\d\\dq\\s)|(?<=\\d\\dq)(\\d\\d))", "|",                 # "##" after "##q" or
+      "((?<=')(\\d\\d))", "|",                                       # "##" after "'" or
+      "((?<=fy\\s)(\\d\\d)|(?<=fy)(\\d\\d))"                         # "##" after "fy " or "fy".
+    ))),
+    # Clean up quarter and year.
+    quarter = as.numeric(quarter),
     year = str_pad(year, 3, side = c("left"), pad = "0"),
     year = str_pad(year, 4, side = c("left"), pad = "2"),
     year = as.numeric(year),
-    quarter = str_replace_all(quarter, "q", ""),
-    quarter = str_replace_all(quarter, "first|1st", "1"),
-    quarter = str_replace_all(quarter, "second|2nd", "2"),
-    quarter = str_replace_all(quarter, "third|3rd", "3"),
-    quarter = str_replace_all(quarter, "fourth|4th", "4"),
-    quarter = as.numeric(quarter)
-  )
-  # filter(!is.na(quarter)) %>%
-  # select(title, call_date, year, quarter) %>%
-  # as.data.frame() %>% 
-  # write_csv(here::here("Private", "test.csv"))
-
-# Confirm that we can filter on "Abstract|Event Brief" in title to 
-# remove most duplicate earnings calls.
-call_data %>% 
-  count(gvkey, call_date) %>%
-  filter(n != 1) %>%
-  left_join(call_data, by = c("gvkey", "call_date")) %>% 
-  select(gvkey, call_date, title) %>% 
-  as.data.frame() %>% 
-  write_csv(here::here("Private", "filter_test_02.csv"))
-
-call_data
+    year = ifelse(year > 2021, NA, year),
+    # Quarters 1 and 2 have a FY = calendar year - 1, otherwise we use the calendar year.
+    year = ifelse(is.na(year) & quarter %in% c(1, 2), lubridate::year(call_date) - 1, year),
+    year = ifelse(is.na(year) & quarter %in% c(3, 4), lubridate::year(call_date), year)
+  ) %>%
+  drop_na(quarter) %>%
+  drop_na(year)
 
 # Firm Performance --------------------------------------------------------
-# Does the assumption of 2000 being the earliest earnings call hold?
-call_data %>% arrange(call_date)
-
 # Create a plain text file (.txt) with one GVKEY code per line
 # for pulling quarterly revenue data from Computstat.
 call_data %>% 
