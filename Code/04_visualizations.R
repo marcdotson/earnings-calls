c4526804fe8b7647fc92f8932de9c6c83ed1344c
wtrumanrose <wtrumanrose@gmail.com>
2022-06-17 18:00:59 -0600

    First few visualizations, some data wrangling included as well which can be moved back to the data wrangling file later.

--- Code/04_visualizations.R
+++ Code/04_visualizations.R
@@ -1,125 +1,114 @@
 # Mostly unsupervised visualizations.
 
+library(tidyverse)
+library(lubridate)
 
-# reimport for fun
-call_data <- read_rds(here::here("Data", "call_data.rds")) %>%
+# Import marketing term lists, resave as .rds files
+
+clmd <- read_csv(here::here("Data", "clmd.csv")) %>% mutate(word= str_to_lower(word))
+lnm <- read_csv(here::here("Data", "marketing_words.csv")) %>% 
+  mutate(word= str_to_lower(word))
+
+
+write_rds(clmd, here::here("Data", "clmd.rds"))
+write_rds(lnm, here::here("Data", "lnm.rds"))
+
+
+clmd <- read_rds(here::here("Data", "clmd.rds"))
+lnm <- read_rds(here::here("Data", "lnm.rds"))
+
+
+# Import call data and call tokens
+call_data <- read_rds(here::here("Data", "call_data.rds")) %>% 
   mutate(rowID = row_number()) %>% 
-  select(year, rowID)
+  select(-text)
+call_tokens <- read_csv(here::here("Data", "call_tokens.csv"))
 
-call_tokens <- call_tokens %>% 
-  left_join(call_data, by = "rowID")
 
-# Overall word count.
+call_data <- call_data %>% mutate(rowID = row_number()) %>% select(-text)
+
+call_tokens <- call_tokens %>% left_join(call_data, by="rowID")
+
+lnm_tokens <- call_tokens %>%
+  semi_join(lnm, by="word") %>%
+  group_by(rowID) %>%
+  count() %>% 
+  rename(marketing_words=n) %>% 
+  left_join(call_data, by="rowID") %>% rename(overall_words=n)
+
+clmd_tokens <- call_tokens %>%
+  semi_join(clmd, by="word") %>%
+  group_by(rowID) %>%
+  count() %>% 
+  rename(marketing_words=n) %>% 
+  left_join(call_data, by="rowID") %>% rename(overall_words=n)
+
+write_rds(lnm_tokens, here::here("Data", "lnm_tokens.rds"))
+write_rds(clmd_tokens, here::here("Data", "clmd_tokens.rds"))
+
+
+
+# Visualizations
+
+# Marketing word proportion--CLMD
+clmd_tokens %>% filter(revenue!=0) %>%  ungroup() %>%
+  unite(year_quarter, year, quarter, sep=":") %>% 
+  mutate(year_quarter = yq(year_quarter),
+         revenue = as.double(revenue),
+         word_prop = marketing_words/overall_words) %>% 
+  ggplot(aes(x=year_quarter, y=word_prop)) +
+  geom_point(size=0.5)+
+  geom_quantile(size=0.5) +
+  geom_smooth(method=lm)
+
+# Revenue; both should be identical
+clmd_tokens %>% filter(revenue!=0) %>%  ungroup() %>%
+  unite(year_quarter, year, quarter, sep=":") %>% 
+  mutate(year_quarter = yq(year_quarter),
+         revenue = as.double(revenue),
+         word_prop = marketing_words/overall_words) %>% 
+  ggplot(aes(x=year_quarter, y=revenue)) +
+  geom_point(size=0.5)+
+  geom_quantile(size=0.5) +
+  geom_smooth(method=lm)
+
+
+
+# Marketing word proportion--L&M
+
+lnm_tokens %>% filter(revenue!=0) %>%  ungroup() %>%
+  unite(year_quarter, year, quarter, sep=":") %>% 
+  mutate(year_quarter = yq(year_quarter),
+         revenue = as.double(revenue),
+         word_prop = marketing_words/overall_words) %>% 
+  ggplot(aes(x=year_quarter, y=word_prop)) +
+  geom_point(size=0.5)+
+  geom_quantile(size=0.5) +
+  geom_smooth(method=lm)
+
+
+
+# Revenue; both should be identical
+lnm_tokens %>% filter(revenue!=0) %>%  ungroup() %>%
+  unite(year_quarter, year, quarter, sep=":") %>% 
+  mutate(year_quarter = yq(year_quarter),
+         revenue = as.double(revenue),
+         word_prop = marketing_words/overall_words) %>% 
+  ggplot(aes(x=year_quarter, y=revenue)) +
+  geom_point(size=0.5)+
+  geom_quantile(size=0.5) +
+  geom_smooth(method=lm)
+
 
 
 # Word count, no stop words
-word_tot %>% 
-  select(word, totals) %>%
-  ungroup %>% 
-  distinct() %>% 
-  arrange(desc(totals)) %>%
-  slice(1:30) %>% 
-  mutate(word = fct_reorder(word, totals)) %>%
-  ggplot(aes(x = totals, y = word)) +
-  geom_col() 
+
   
 
 
 # Overall word count by year.
 
-lm_stopwords %>% 
-  group_by(year) %>% 
-  slice(1:10) %>% 
-  ungroup %>% 
-  mutate(word = reorder_within(word, n, year)) %>%
-  ggplot(aes(x = n, y = word)) +
-  geom_col() +
-  facet_wrap(~ year, scales="free") +
-  scale_y_reordered()
-
-
-word_tokens %>% 
-  inner_join(terms) %>% 
-  count(word) %>%
-  arrange(desc(n)) %>% 
-  ggplot(aes(x = n, y = word)) +
-  geom_col()
-
-call_tokens <- read_csv(here::here("Data", "call_tokens.csv"))
-
-call_tokens %>% 
-  inner_join(terms) %>% 
-  count(word) %>%
-  arrange(desc(n)) %>% 
-  slice(1:25) %>% 
-  mutate(word = fct_reorder(word, n)) %>% 
-  ggplot(aes(x = n, y = word)) +
-  geom_col()
-
-
-
-
-
-# top 10 marketing terms by year
-call_tokens %>% 
-  inner_join(terms) %>% 
-  group_by(year) %>% 
-  count(word) %>%
-  arrange(desc(n)) %>% 
-  slice(1:10) %>% 
-  ungroup %>% 
-  mutate(word = reorder_within(word, n, year)) %>%
-  ggplot(aes(x = n, y = word)) +
-  geom_col() +
-  facet_wrap(~ year, scales="free") +
-  scale_y_reordered()
-
-
-
-# top 10 words by year
-call_tokens %>% 
-  group_by(year) %>% 
-  count(word) %>%
-  arrange(desc(n)) %>% 
-  slice(1:10) %>% 
-  ungroup %>% 
-  mutate(word = reorder_within(word, n, year)) %>%
-  ggplot(aes(x = n, y = word)) +
-  geom_col() +
-  facet_wrap(~ year, scales="free") +
-  scale_y_reordered()
-
-# 25 overall word count
-call_tokens %>% 
-  count(word) %>%
-  arrange(desc(n)) %>% 
-  slice(1:25) %>% 
-  mutate(word = fct_reorder(word, n)) %>% 
-  ggplot(aes(x = n, y = word)) +
-  geom_col()
-
-
 # Word embeddings visualizations or as part of modeling?
 
 
 
-# More code from SMLTA
-
-tidy_word_vectors %>%
-  filter(dimension <= 24) %>%
-  group_by(dimension) %>%
-  top_n(8, abs(value)) %>%
-  ungroup() %>%
-  mutate(item1 = reorder_within(item1, value, dimension)) %>%
-  ggplot(aes(item1, value, fill = dimension)) +
-  geom_col(alpha = 0.8, show.legend = FALSE) +
-  facet_wrap(~dimension, scales = "free_y", ncol = 4) +
-  scale_x_reordered() +
-  coord_flip() +
-  labs(
-    x = NULL,
-    y = "Value",
-    title = "Apple Earnings Calls",
-    subtitle = paste("Top words contributing to the components that explain",
-                     "the most variation")
-  )
