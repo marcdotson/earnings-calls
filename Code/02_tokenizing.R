f1962b50f068ab060202e05bd4fe8784a75d8a58
Marc Dotson <marc_dotson@byu.edu>
2022-06-27 16:29:24 -0600

    Review of 02_tokenizing

--- Code/02_tokenizing.R
+++ Code/02_tokenizing.R
@@ -8,11 +8,13 @@ call_data <- read_rds(here::here("Data", "call_data.rds")) |>
   mutate(
     title_text = str_c(title, text, " "),                 # Combine title and text.
     title_text = str_replace_all(text, "[:punct:]", " ")  # Strip punctuation to deal with contractions.
-  ) |> select(id, title_text)                             # Select only id and text to reduce memory demand
+  ) |> 
+  select(id, title_text)                                  # Select only id and title_text to reduce memory demand.
 
+call_data
 
-# Mutate function takes a bit to run, so I saved it
-# write_rds(call_data, here::here("Data", "temp_call.rds"))
+# # Mutate function takes a bit to run, so we save it as temp_call.rds.
+# # write_rds(call_data, here::here("Data", "temp_call.rds"))
 # call_data <- read_rds(here::here("Data", "temp_call.rds"))
 
 # Import L&M generic stop words.
@@ -32,7 +34,7 @@ for (i in seq_along(1:num_splits)) {
   
   # Tokenize and assign to a separate object each iteration.
   tokens <- call_data |> 
-    # Chunking the data
+    # Slice the data.
     slice(start:end) |> 
     # Strip punctuation to deal with contractions.
     unnest_tokens(word, title_text, strip_punct = TRUE) |> 
@@ -50,70 +52,53 @@ for (i in seq_along(1:num_splits)) {
     ) |> 
     # Remove generic stop words.
     anti_join(generic_stopwords) |> 
-    # Save tokens with associated id
+    # Save tokens with associated id.
     write_rds(here::here("Data", tokens_name))
 
-    # Remove objects to keep memory demand low
+    # Remove objects to reduce memory demand.
     rm(tokens, tokens_name)
 }
-
-
-# tokens_1
 
 # Bind Data ---------------------------------------------------------------
 # Remove data that no longer needs to be held in memory.
 rm(call_data, generic_stopwords)
 
-
-# Reimport complete call_data except for text.
+# Re-import complete call_data except for text.
 call_data <- read_rds(here::here("Data", "call_data.rds")) |> 
   select(
     id, gvkey, sector, group, industry, sub_industry, 
-    call_date, year, quarter, revenue, title)
+    call_date, year, quarter, revenue, title
+  )
 
-# Create word_tokens tibble
+# Bind sliced tokenized data.
 word_tokens <- NULL
-
-# Binding separate sets
 for (i in 1:num_splits) {
-  # Recreate saved file names
+  # Recreate saved file names.
   tokens_name <- str_c("tokens_", i, ".rds")
   
-  # Import saved files
+  # Import saved files.
   tokens <- read_rds(here::here("Data", tokens_name)) |>
     group_by(id) |> 
-    # Nesting words by id, hopefully reducing memory demand. This can always be undone with unnest()
+    # Nesting words by id, reducing memory demand (this can always be undone with unnest()).
     nest(words = c(word)) |> 
-    # Rejoin firm data
+    # Rejoin firm data.
     left_join(call_data, by = "id")
   
-  # Bind chunked data to main data frame
+  # Bind sliced data to main data frame.
   word_tokens <- word_tokens |>
     bind_rows(tokens)
   
-  # Remove rows from call data that have been joined. This reduces memory demand and highlights any 
-  # documents which might've been missed
+  # Remove rows from call_data that have been joined. This reduces memory demand and 
+  # highlights any documents which might have been missed.
   call_data <- call_data |> anti_join(word_tokens, by = "id")
   
-  # Deleting chunked token files
-  unlink(here::here("Data",tokens_name))
+  # # Delete sliced tokenized data.
+  # unlink(here::here("Data", tokens_name))
 }
 
-# Bind the separate sets.
-# word_tokens <- NULL
-# for (i in seq_along(1:num_splits)) {
-#   word_tokens <- bind_rows(word_tokens, get(str_c("tokens_", i)))
-# }
-
+# Ungroup word_tokens.
 word_tokens <- word_tokens |> ungroup()
 
-# Remove data that no longer needs to be held in memory.
-# rm(list = setdiff(ls(), "word_tokens"))
-
-# Write the word tokens.
+# Write word_tokens.
 write_rds(word_tokens, here::here("Data", "word_tokens.rds"))
-
-
-
-word_tokens <- read_rds(here::here("Data", "word_tokens.rds"))
 
