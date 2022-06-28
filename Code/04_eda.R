4b410100abcf2b24f881636ba73903993f0612be
Marc Dotson <marc_dotson@byu.edu>
2022-06-27 19:43:00 -0600

    Cleaning up EDA

--- Code/04_eda.R
+++ Code/04_eda.R
@@ -1,67 +1,190 @@
-# Mostly unsupervised visualizations.
-
+# Visualize Word Counts ---------------------------------------------------
+# Load packages.
 library(tidyverse)
 library(lubridate)
-library(edgar) # Includes the LMMasterDictionary.
+library(edgar) # Includes the Loughran and McDonald's master dictionary.
 
-# Import marketing term lists, resave as .rds files
+word_tokens <- read_rds(here::here("Data", "word_tokens.rds"))  # Nested word tokens.
+clmd <- read_rds(here::here("Data", "clmd.rds"))                # Common Language Marketing Dictionary terms.
+lnm <- read_rds(here::here("Data", "lnm.rds"))                  # Loughran and McDonald dictionary terms.
+unnest_word_tokens <- word_tokens |> unnest(cols = words)       # Unnest word tokens.
+overall_word_counts <- unnest_word_tokens |> count(word)        # Compute overall word tokens.
+sector_word_counts <- unnest_word_tokens |> count(word, sector) # Compute word tokens by sector.
 
-clmd <- read_csv(here::here("Data", "clmd.csv")) %>% mutate(word= str_to_lower(word))
-lnm <- read_csv(here::here("Data", "marketing_words.csv")) %>% 
-  mutate(word= str_to_lower(word))
+# Save and load intermediate steps, as needed.
+write_rds(lnm_tokens, here::here("Data", "overall_word_counts.rds"))
+write_rds(clmd_tokens, here::here("Data", "sector_word_counts.rds"))
+# overall_word_counts <- read_rds(here::here("Data", "overall_word_counts.rds"))
+# sector_word_counts <- read_rds(here::here("Data", "sector_word_counts.rds"))
+
+# Top 10 overall terms.
+overall_word_counts |> 
+  arrange(desc(n)) |> 
+  slice(1:10) |> 
+  mutate(word = fct_reorder(word, n)) |> 
+  ggplot(aes(x = n, y = word)) +
+  geom_col()
+
+ggsave(
+  filename = here::here("Figures", "overall-terms_top-10.png"),
+  width = 4, height = 7, units = "in"
+)
+
+# Top 25 overall terms.
+overall_word_counts |> 
+  arrange(desc(n)) |> 
+  slice(1:25) |> 
+  mutate(word = fct_reorder(word, n)) |> 
+  ggplot(aes(x = n, y = word)) +
+  geom_col()
+
+ggsave(
+  filename = here::here("Figures", "overall-terms_top-25.png"),
+  width = 4, height = 7, units = "in"
+)
+
+# Top 10 marketing terms using clmd.
+overall_word_counts
+  semi_join(clmd, by = "word") |> 
+  arrange(desc(n)) |> 
+  slice(1:10) |> 
+  mutate(word = fct_reorder(word, n)) |> 
+  ggplot(aes(x = n, y = word)) +
+  geom_col()
+
+ggsave(
+  filename = here::here("Figures", "marketing-terms_top-10_clmd.png"),
+  width = 4, height = 7, units = "in"
+)
+
+# Top 25 marketing terms using clmd.
+overall_word_counts |> 
+  semi_join(clmd, by = "word") |> 
+  arrange(desc(n)) |> 
+  slice(1:25) |> 
+  mutate(word = fct_reorder(word, n)) |> 
+  ggplot(aes(x = n, y = word)) +
+  geom_col()
+
+ggsave(
+  filename = here::here("Figures", "marketing-terms_top-25_clmd.png"),
+  width = 4, height = 7, units = "in"
+)
+
+# Top 10 marketing terms using lnm.
+unnest_word_tokens |> 
+  semi_join(lnm, by = "word") |> 
+  count(word) |> 
+  arrange(desc(n)) |> 
+  slice(1:10) |> 
+  mutate(word = fct_reorder(word, n)) |> 
+  ggplot(aes(x = n, y = word)) +
+  geom_col()
+
+ggsave(
+  filename = here::here("Figures", "marketing-terms_top-10_lnm.png"),
+  width = 4, height = 7, units = "in"
+)
+
+# Top 25 marketing terms using lnm.
+unnest_word_tokens |> 
+  semi_join(lnm, by = "word") |> 
+  count(word) |> 
+  arrange(desc(n)) |> 
+  slice(1:25) |> 
+  mutate(word = fct_reorder(word, n)) |> 
+  ggplot(aes(x = n, y = word)) +
+  geom_col()
+
+ggsave(
+  filename = here::here("Figures", "marketing-terms_top-25_lnm.png"),
+  width = 4, height = 7, units = "in"
+)
 
 
-write_rds(clmd, here::here("Data", "clmd.rds"))
-write_rds(lnm, here::here("Data", "lnm.rds"))
 
 
-clmd <- read_rds(here::here("Data", "clmd.rds"))
-lnm <- read_rds(here::here("Data", "lnm.rds"))
 
 
-# Import call data and word tokens
+fit_lda6 %>% 
+  tidy(matrix = "beta") %>%
+  group_by(topic) %>% 
+  top_n(10, beta) %>%
+  ungroup() %>%
+  mutate(term = reorder_within(term, beta, topic)) %>%
+  ggplot(aes(x = beta, y = term, fill = as.factor(topic))) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ topic, scales = "free") +
+  scale_y_reordered()
 
-call_data <- read_rds(here::here("Data", "call_data.rds"))
+# Remove data that no longer needs to be held in memory.
+rm(unnest_word_tokens, overall_word_counts, sector_word_counts)
 
-word_tokens <- read_rds(here::here("Data", "word_tokens.rds")) |> ungroup()
-
-
-lnm_tokens <- word_tokens %>%
-  # Making year_quarter variable for easier time series
-  unite(year_quarter, year, quarter, sep=":", remove = FALSE) %>% 
+# Visualize Revenue Over Time ---------------------------------------------
+# Count marketing words based on lnm terms.
+lnm_tokens <- word_tokens |> 
+  # Making year_quarter variable for easier time series.
+  unite(year_quarter, year, quarter, sep=":", remove = FALSE) |> 
   mutate(
-    # Counting rows within the nested data
+    # Counting rows within the nested data.
     overall_words = map_dbl(words, ~.x |> nrow()),
-    # Counting rows within the nested data after joining dictionary
+    # Counting rows within the nested data after joining the dictionary.
     marketing_words = map_dbl(words, ~.x |> semi_join(lnm, by = "word") |> nrow()),
-    # Formatting year_quarter
+    # Formatting year_quarter.
     year_quarter = yq(year_quarter), 
-    # Converting revenue to double
+    # Converting revenue to double.
     revenue = as.double(revenue),
-    # Word proportions
-    word_prop = marketing_words/overall_words)
+    # Word proportions.
+    word_prop = marketing_words / overall_words
+  )
 
-clmd_tokens <- word_tokens %>%
-  # Making year_quarter variable for easier time series
-  unite(year_quarter, year, quarter, sep=":",  remove = FALSE) %>% 
+lnm_tokens
+
+# Count marketing words based on clmd terms.
+clmd_tokens <- word_tokens |> 
+  # Making year_quarter variable for easier time series.
+  unite(year_quarter, year, quarter, sep=":", remove = FALSE) |> 
   mutate(
-    # Counting rows within the nested data
+    # Counting rows within the nested data.
     overall_words = map_dbl(words, ~.x |> nrow()),
-    # Counting rows within the nested data after joining dictionary
+    # Counting rows within the nested data after joining the dictionary.
     marketing_words = map_dbl(words, ~.x |> semi_join(clmd, by = "word") |> nrow()),
-    # Formatting year_quarter
+    # Formatting year_quarter.
     year_quarter = yq(year_quarter), 
-    # Converting revenue to double
+    # Converting revenue to double.
     revenue = as.double(revenue),
-    # Word proportions
-    word_prop = marketing_words/overall_words)
+    # Word proportions.
+    word_prop = marketing_words / overall_words
+  )
 
-# Writing and reloading token counts
-write_rds(lnm_tokens, here::here("Data", "lnm_tokens.rds"))
-write_rds(clmd_tokens, here::here("Data", "clmd_tokens.rds"))
+clmd_tokens
 
-clmd_tokens <- read_rds(here::here("Data", "clmd_tokens.rds"))
-lnm_tokens <- read_rds(here::here("Data", "lnm_tokens.rds"))
+# # Save and load intermediate steps, as needed.
+# # write_rds(lnm_tokens, here::here("Data", "lnm_tokens.rds"))
+# # write_rds(clmd_tokens, here::here("Data", "clmd_tokens.rds"))
+# clmd_tokens <- read_rds(here::here("Data", "clmd_tokens.rds"))
+# lnm_tokens <- read_rds(here::here("Data", "lnm_tokens.rds"))
+
+# L&M marketing word proportions over time.
+lnm_tokens |> 
+  filter(revenue != 0) |>  
+  ggplot(aes(x=year_quarter, y=word_prop)) +
+  geom_point(size=0.5)+
+  geom_quantile(size=0.5) +
+  geom_smooth(method=lm) +
+  ggtitle()
+
+# Save.
+
+# Revenue; both should be identical
+lnm_tokens %>% filter(revenue!=0) %>% 
+  ggplot(aes(x=year_quarter, y=revenue)) +
+  geom_point(size=0.5)+
+  geom_quantile(size=0.5) +
+  geom_smooth(method=lm)
+
+# Save.
+
 # Marketing word proportion--CLMD
 clmd_tokens %>% filter(revenue!=0) %>%
   ggplot(aes(x=year_quarter, y=word_prop)) +
@@ -77,23 +200,6 @@ clmd_tokens %>% filter(revenue!=0) %>%
   geom_smooth(method=lm)
 
 
-
-# Marketing word proportion--L&M
-
-lnm_tokens %>% filter(revenue!=0) %>% 
-  ggplot(aes(x=year_quarter, y=word_prop)) +
-  geom_point(size=0.5)+
-  geom_quantile(size=0.5) +
-  geom_smooth(method=lm)
-
-
-
-# Revenue; both should be identical
-lnm_tokens %>% filter(revenue!=0) %>% 
-  ggplot(aes(x=year_quarter, y=revenue)) +
-  geom_point(size=0.5)+
-  geom_quantile(size=0.5) +
-  geom_smooth(method=lm)
 
 
 
@@ -138,7 +244,6 @@ test12 <- lnm |>
 
 # Overall word count by year.
 
-# Word embeddings visualizations or as part of modeling?
 
 
 
