b30e4a636c2de079aaad3f2a393fa0f354ea7738
Marc Dotson <marc_dotson@byu.edu>
2022-07-05 13:20:05 -0600

    Cleaning up word_embeddings

--- Code/03_word-embeddings.R
+++ Code/03_word-embeddings.R
@@ -5,27 +5,29 @@ library(tidytext)
 library(widyr)
 library(furrr)
 
-#slider creates slide windows; used for word embeddings
-#widyr does word embeddings; supposedly this package isn't good for academic purposes
-#furrr enables parallel processing
-#text2vec, word2vec, and FastText provide alternative options for embeddings
+# Import nested word tokens.
+word_tokens <- read_rds(here::here("Data", "word_tokens.rds"))
 
-# Word embeddings seem to occupy a space somewhere between data wrangling and
-# modeling. They are computationally intensive and require a fair bit of
-# judgment. The process used to make word embeddings is outlined in SMLTA
-# I won't use the stemmed words for this, unless I find a compelling reason to.
-# No stopwords removed, contractions are gone though
+word_tokens
 
-# Add counts, filter by 10 or up, nest words 
+##########
+tidy_complaints <- complaints %>%
+  select(complaint_id, consumer_complaint_narrative) %>%
+  unnest_tokens(word, consumer_complaint_narrative) %>%
+  add_count(word) %>%
+  filter(n >= 50) %>%
+  select(-n)
 
-call_tokens <- read_csv(here::here("Data", "call_tokens.csv"))
-
-call_tokens <- call_tokens %>% 
+nested_words <- tidy_complaints %>%
   nest(words = c(word))
 
+nested_words
+##########
+
+
+
+
 # Creating slide windows, which are used to calculate skipgram probabilities
-
-
 slide_windows <- function(tbl, window_size) {
   skipgrams <- slider::slide(
     tbl, 
@@ -54,9 +56,10 @@ plan(multisession)  ## for parallel processing
 # Not exactly sure how to explain what I'm doing with this, which isn't great
 # 8L for 8 cores! Didn't take long to run though, even if you have 4 cores
 
-# COmmenting out for now, will revisit later
+# window_size
+# nv
 
-tidy_pmi <- call_tokens %>%
+tidy_pmi <- word_tokens %>%
   mutate(words = future_map(words, slide_windows, 4L)) %>%
   unnest(words) %>%
   unite(window_id, rowID, window_id) %>%
@@ -64,7 +67,6 @@ tidy_pmi <- call_tokens %>%
 
 
 # Creating the vectors
-
 tidy_word_vectors <- tidy_pmi %>%
   widely_svd(
     item1, item2, pmi,
@@ -73,25 +75,5 @@ tidy_word_vectors <- tidy_pmi %>%
 
 # # Hurray!
 
+# Save word_embeddings...
 
-# # Function from SMLTA to find the nearest neighbor of words
-# 
-# nearest_neighbors <- function(df, token) {
-#   df %>%
-#     widely(
-#       ~ {
-#         y <- .[rep(token, nrow(.)), ]
-#         res <- rowSums(. * y) / 
-#           (sqrt(rowSums(. ^ 2)) * sqrt(sum(.[token, ] ^ 2)))
-#         
-#         matrix(res, ncol = 1, dimnames = list(x = names(res)))
-#       },
-#       sort = TRUE
-#     )(item1, dimension, value) %>%
-#     select(-item2)
-# }
-# 
-# 
-# nearest_neighbors(tidy_word_vectors, "sales")
-# 
-# nearest_neighbors(tidy_word_vectors, "drive")
