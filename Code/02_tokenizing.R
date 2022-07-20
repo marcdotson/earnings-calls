082cec0b6d4834066314a4f305a39cbf1ba65ba8
wtrumanrose <wtrumanrose@gmail.com>
2022-07-20 05:06:17 -0600

    Copywrite blurb cleanup

--- Code/02_tokenizing.R
+++ Code/02_tokenizing.R
@@ -6,12 +6,89 @@ library(tidytext)
 # Import call data.
 call_data <- read_rds(here::here("Data", "call_data.rds")) |>
   mutate(
-    title_text = str_c(title, text, " "),                 # Combine title and text.
-    title_text = str_replace_all(text, "[:punct:]", " ")  # Strip punctuation to deal with contractions.
+    title_text = str_c(title, text, " ")                 # Combine title and text.
+   # title_text = str_replace_all(text, "[:punct:]", " ")  # Strip punctuation to deal with contractions.
   ) |> 
   select(id, title_text)                                  # Select only id and title_text to reduce memory demand.
 
 call_data
+
+
+# Not sure where to put this, it might be better in the 01_data-wrangling.
+# I was trying to cut out superfluous/confounding language within the transcripts,
+# initially I was looking into cutting out names, but then I realized that most
+# of the transcripts have some sort of copywrite blurb at the end. I tried
+# to do some regex magic, but gave up and did it the naive way.
+# I only found 5 different types of blurbs, there may be more but this covers
+# the vast majority of transcripts. The other transcripts just seem to end
+# with the operator thanking everyone and wishing them a good day.
+
+# Not sure the best way to do this, as 4 of the 5 blurbs are rather long.
+blurb1 <- "CCBN reserves the right to make changes to documents, content, or other information on this web site without obligation to notify any person of such changes. In the conference calls upon which Event Transcripts are based, companies may make projections or other forward-looking statements regarding a variety of items. Such forward-looking statements are based upon current expectations and involve risks and uncertainties. Actual results may differ materially from those stated in any forward-looking statement based on a number of important factors and risks, which are more specifically identified in the companies' most recent SEC filings. Although the companies may indicate and believe that the assumptions underlying the forward-looking statements are reasonable, any of the assumptions could prove inaccurate or incorrect and, therefore, there can be no assurance that the results contemplated in the forward-looking statements will be realized. THE INFORMATION CONTAINED IN EVENT TRANSCRIPTS IS A TEXTUAL REPRESENTATION OF THE APPLICABLE COMPANY'S CONFERENCE CALL AND WHILE EFFORTS ARE MADE TO PROVIDE AN ACCURATE TRANSCRIPTION, THERE MAY BE MATERIAL ERRORS, OMISSIONS, OR INACCURACIES IN THE REPORTING OF THE SUBSTANCE OF THE CONFERENCE CALLS. IN NO WAY DOES CCBN ASSUME ANY RESPONSIBILITY FOR ANY INVESTMENT OR OTHER DECISIONS MADE BASED UPON THE INFORMATION PROVIDED ON THIS WEB SITE OR IN ANY EVENT TRANSCRIPT. USERS ARE ADVISED TO REVIEW THE APPLICABLE COMPANY'S CONFERENCE CALL ITSELF AND THE APPLICABLE COMPANY'S SEC FILINGS BEFORE MAKING ANY INVESTMENT OR OTHER DECISIONS."
+
+blurb2 <- "Thomson Financial reserves the right to make changes to documents, content, or other information on this web site without obligation to notify any person of such changes. In the conference calls upon which Event Transcripts are based, companies may make projections or other forward-looking statements regarding a variety of items. Such forward-looking statements are based upon current expectations and involve risks and uncertainties. Actual results may differ materially from those stated in any forward-looking statement based on a number of important factors and risks, which are more specifically identified in the companies' most recent SEC filings. Although the companies may indicate and believe that the assumptions underlying the forward-looking statements are reasonable, any of the assumptions could prove inaccurate or incorrect and, therefore, there can be no assurance that the results contemplated in the forward-looking statements will be realized. THE INFORMATION CONTAINED IN EVENT TRANSCRIPTS IS A TEXTUAL REPRESENTATION OF THE APPLICABLE COMPANY'S CONFERENCE CALL AND WHILE EFFORTS ARE MADE TO PROVIDE AN ACCURATE TRANSCRIPTION, THERE MAY BE MATERIAL ERRORS, OMISSIONS, OR INACCURACIES IN THE REPORTING OF THE SUBSTANCE OF THE CONFERENCE CALLS. IN NO WAY DOES THOMSON FINANCIAL OR THE APPLICABLE COMPANY OR THE APPLICABLE COMPANY ASSUME ANY RESPONSIBILITY FOR ANY INVESTMENT OR OTHER DECISIONS MADE BASED UPON THE INFORMATION PROVIDED ON THIS WEB SITE OR IN ANY EVENT TRANSCRIPT. USERS ARE ADVISED TO REVIEW THE APPLICABLE COMPANY'S CONFERENCE CALL ITSELF AND THE APPLICABLE COMPANY'S SEC FILINGS BEFORE MAKING ANY INVESTMENT OR OTHER DECISIONS."
+
+# blurb3 has an extra space at the start of a sentence
+blurb3 <- "Thomson Financial reserves the right to make changes to documents, content, or other information on this web site without obligation to notify any person of such changes.  In the conference calls upon which Event Transcripts are based, companies may make projections or other forward-looking statements regarding a variety of items. Such forward-looking statements are based upon current expectations and involve risks and uncertainties. Actual results may differ materially from those stated in any forward-looking statement based on a number of important factors and risks, which are more specifically identified in the companies' most recent SEC filings. Although the companies may indicate and believe that the assumptions underlying the forward-looking statements are reasonable, any of the assumptions could prove inaccurate or incorrect and, therefore, there can be no assurance that the results contemplated in the forward-looking statements will be realized.  THE INFORMATION CONTAINED IN EVENT TRANSCRIPTS IS A TEXTUAL REPRESENTATION OF THE APPLICABLE COMPANY'S CONFERENCE CALL AND WHILE EFFORTS ARE MADE TO PROVIDE AN ACCURATE TRANSCRIPTION, THERE MAY BE MATERIAL ERRORS, OMISSIONS, OR INACCURACIES IN THE REPORTING OF THE SUBSTANCE OF THE CONFERENCE CALLS. IN NO WAY DOES THOMSON FINANCIAL OR THE APPLICABLE COMPANY OR THE APPLICABLE COMPANY ASSUME ANY RESPONSIBILITY FOR ANY INVESTMENT OR OTHER DECISIONS MADE BASED UPON THE INFORMATION PROVIDED ON THIS WEB SITE OR IN ANY EVENT TRANSCRIPT. USERS ARE ADVISED TO REVIEW THE APPLICABLE COMPANY'S CONFERENCE CALL ITSELF AND THE APPLICABLE COMPANY'S SEC FILINGS BEFORE MAKING ANY INVESTMENT OR OTHER DECISIONS."
+
+blurb4 <- "CCBN reserves the right to make changes to documents, content, or other information on this web site without obligation to notify any person of such changes.  In the conference calls upon which Event Transcripts are based, companies may make projections or other forward-looking statements regarding a variety of items. Such forward-looking statements are based upon current expectations and involve risks and uncertainties. Actual results may differ materially from those stated in any forward-looking statement based on a number of important factors and risks, which are more specifically identified in the companies' most recent SEC filings. Although the companies may indicate and believe that the assumptions underlying the forward-looking statements are reasonable, any of the assumptions could prove inaccurate or incorrect and, therefore, there can be no assurance that the results contemplated in the forward-looking statements will be realized.  THE INFORMATION CONTAINED IN EVENT TRANSCRIPTS IS A TEXTUAL REPRESENTATION OF THE APPLICABLE COMPANY'S CONFERENCE CALL AND WHILE EFFORTS ARE MADE TO PROVIDE AN ACCURATE TRANSCRIPTION, THERE MAY BE MATERIAL ERRORS, OMISSIONS, OR INACCURACIES IN THE REPORTING OF THE SUBSTANCE OF THE CONFERENCE CALLS. IN NO WAY DOES CCBN ASSUME ANY RESPONSIBILITY FOR ANY INVESTMENT OR OTHER DECISIONS MADE BASED UPON THE INFORMATION PROVIDED ON THIS WEB SITE OR IN ANY EVENT TRANSCRIPT. USERS ARE ADVISED TO REVIEW THE APPLICABLE COMPANY'S CONFERENCE CALL ITSELF AND THE APPLICABLE COMPANY'S SEC FILINGS BEFORE MAKING ANY INVESTMENT OR OTHER DECISIONS. Copyright 200[0-9], CCBN, Inc. All Rights Reserved."
+
+blurb5 <- "NO PORTION OF THIS TRANSCRIPTION MAY BE COPIED, SOLD OR RETRANSMITTED WITHOUT THE EXPRESS WRITTEN AUTHORITY OF FDCH-eMedia, Inc."
+
+
+# Saving the number of transcripts containing each blurb for validity.
+
+# 14286 transcripts
+blurb1_transcripts <- call_data |>
+  mutate(blurb = str_detect(title_text, blurb1)) |> 
+  summarise(n= sum(blurb==TRUE))
+
+blurb1_transcripts
+
+# 108718 transcripts
+blurb2_transcripts <- call_data |>
+  mutate(blurb = str_detect(title_text, blurb2)) |> 
+  summarise(n= sum(blurb==TRUE))
+
+blurb2_transcripts
+
+# 4662 transcripts
+blurb3_transcripts <- call_data |>
+  mutate(blurb = str_detect(title_text, blurb3)) |> 
+  summarise(n= sum(blurb==TRUE))
+
+blurb3_transcripts
+
+# 1842 transcripts
+blurb4_transcripts <- call_data |>
+  mutate(blurb = str_detect(title_text, blurb4)) |> 
+  summarise(n= sum(blurb==TRUE))
+
+blurb4_transcripts
+
+# 571 transcripts
+blurb5_transcripts <- call_data |>
+  mutate(blurb = str_detect(title_text, blurb5)) |> 
+  summarise(n= sum(blurb==TRUE))
+
+blurb5_transcripts
+
+# This should leave 452 transcripts without a copywrite blurb. From looking at a 
+# not-so-random sample of about 20 transcripts, I didn't find any other obvious
+# copywrite blurbs. I also didn't mess with the copywrite parts at the beginning.
+# These blurbs at the end were low-hanging fruit and contain a sizable amount of
+# words, so I figured it was worth rerunning everything to remove them.
+
+# Blurb removal
+call_data <- call_data |>
+  mutate(title_text = str_remove_all(title_text, blurb1)) |>
+  mutate(title_text = str_remove_all(title_text, blurb2)) |>
+  mutate(title_text = str_remove_all(title_text, blurb3)) |>
+  mutate(title_text = str_remove_all(title_text, blurb4)) |>
+  mutate(title_text = str_remove_all(title_text, blurb5))
+
+
+
 
 # # Mutate can take a bit to run, so we can save it as temp_call.rds.
 # # write_rds(call_data, here::here("Data", "temp_call.rds"))
